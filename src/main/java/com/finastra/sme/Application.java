e7ee5863ec4853d9eb7a9e1505c1b0db467cfcb1
Amitesh Ranjan Srivastava <amitesh430@gmail.com>
2021-10-26 14:19:33 +0000

    Initial commit

---
+++ src/main/java/com/finastra/sme/Application.java
@@ -0,0 +1,10 @@
+package com.finastra.sme;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
